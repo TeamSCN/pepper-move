e46e4929934843f07e2f85fb1c6a702d044d57e7
nakkat <nakkat@outlook.com>
2017-11-16 20:22:03 +0900

    Move pepper move and take photos code to json server

---
+++ post_json.py
@@ -0,0 +1,15 @@
+import urllib.request, json
+
+if __name__ == '__main__':
+    url = "http://192.168.1.140:8000"
+    method = "POST"
+    headers = {"Content-Type" : "application/json"}
+
+    # PythonオブジェクトをJSONに変換する
+    obj = {'0':{"x" : 1.0, 'y':0.0, 'theta':0.0}, 'stop_flg': 0}
+    json_data = json.dumps(obj).encode("utf-8")
+
+    # httpリクエストを準備してPOST
+    request = urllib.request.Request(url, data=json_data, method=method, headers=headers)
+    with urllib.request.urlopen(request) as response:
+        response_body = response.read().decode("utf-8")
